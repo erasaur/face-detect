600e7b1f6f8078c47fc2b6afc1570b288d3030b4
Mark Lee <marklee.lhs@gmail.com>
2015-12-30 15:37:06 -0800

    Cleanup

--- lib/face-detect.cpp
+++ lib/face-detect.cpp
@@ -1,53 +1,145 @@
+#include <zmq.hpp>
+#include <iostream>
+#include <string>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
-const char* keys = 
-{
-  "{i|input| |The source image}"
-  "{o|outdir| |The output directory}"
-};
+#define IM_WIDTH 320
+#define IM_HEIGHT 240
 
-int main(int argc, const char** argv)
-{
-  cv::CommandLineParser parser(argc, argv, keys);
-  cv::Mat src = parser.get<std::string>("input");
-  std::string cascade_file = "haarcascade_frontalface_alt.xml";
+static const std::string base64_chars = 
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "0123456789+/";
 
-  cv::CascadeClassifier cascade;
-  if (cascade_file.empty() || !cascade.load(cascade_file))
-  {
-    std::cout << cv::format("Error: cannot load cascade file!\n");
-    return -1;
+static inline bool is_base64 (unsigned char c) {
+  return (isalnum(c) || (c == '+') || (c == '/'));
+}
+std::string base64_encode (unsigned char const* bytes_to_encode, unsigned int in_len) {
+  std::string ret;
+  int i = 0;
+  int j = 0;
+  unsigned char char_array_3[3];
+  unsigned char char_array_4[4];
+
+  while (in_len--) {
+    char_array_3[i++] = *(bytes_to_encode++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for (i = 0; i < 4; i++) {
+        ret += base64_chars[char_array_4[i]];
+      }
+      i = 0;
+    }
   }
 
-  if (src.empty())
-  {
-    std::cout << cv::format("Error: cannot load source image!\n");
-    return -1;
+  if (i) {
+    for (j = i; j < 3; j++) {
+      char_array_3[j] = '\0';
+    }
+
+    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+    char_array_4[3] = char_array_3[2] & 0x3f;
+
+    for (j = 0; (j < i + 1); j++) {
+      ret += base64_chars[char_array_4[j]];
+    }
+
+    while (i++ < 3) {
+      ret += '=';
+    }
   }
 
-  cv::Mat gray;
-  cv::cvtColor(src, gray, CV_BGR2GRAY);
-  cv::equalizeHist(gray, gray);
+  return ret;
+}
 
-  std::vector<cv::Rect> faces;
-  cascade.detectMultiScale(gray, faces, 1.2, 3);
+std::string base64_decode (std::string const& encoded_string) {
+  int in_len = encoded_string.size();
+  int i = 0;
+  int j = 0;
+  int in_ = 0;
+  unsigned char char_array_4[4], char_array_3[3];
+  std::string ret;
 
-  /* std::cout << cv::format("0, %s (%dx%d)\n", infile.c_str(), src.cols, src.rows); */
+  while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+    char_array_4[i++] = encoded_string[in_]; in_++;
+    if (i == 4) {
+      for (i = 0; i < 4; i++) {
+        char_array_4[i] = base64_chars.find(char_array_4[i]);
+      }
 
-  /* cv::Mat src_copy = src.clone(); */
-  for (int i = 0; i < faces.size(); i++)
-  {
-    /* std::string outfile(cv::format("%s/face-%d.jpg", outdir.c_str(), i+1)); */
-    cv::Rect r = faces[i];
-    cv::rectangle(src, r, CV_RGB(0,255,0), 2);
-    /* cv::imwrite(outfile, src_copy(r)); */
-    /* cv::imwrite(infile, src); */
-    /* std::cout << cv::format("%d, %s (%dx%d)\n", i+1, outfile.c_str(), r.width, r.height); */
+      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+      for (i = 0; i < 3; i++) {
+        ret += char_array_3[i];
+      }
+      i = 0;
+    }
   }
-  std::cout << src << std::endl;
 
-  return 0;
+  if (i) {
+    for (j = i; j < 4; j++) {
+      char_array_4[j] = 0;
+    }
+
+    for (j = 0; j < 4; j++) {
+      char_array_4[j] = base64_chars.find(char_array_4[j]);
+    }
+
+    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+    for (j = 0; j < i - 1; j++) {
+      ret += char_array_3[j];
+    }
+  }
+
+  return ret;
+}
+
+int main (int argc, char** argv) {
+  if (argc < 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  zmq::context_t context(1);
+  zmq::socket_t responder(context, ZMQ_REP);
+
+  std::string loc = "tcp://127.0.0.1:";
+  std::string port = argv[1];
+  responder.bind(loc + port);
+  std::cout << "zeromq connected to port " << port << std::endl;
+
+  while (true) {
+    zmq::message_t message;
+    responder.recv(&message);
+
+    void* msg_data = message.data();
+    long msg_size = message.size();
+    char* msg_data_char = (char*) message.data();
+
+    std::string msg_data_str = std::string(static_cast<char*>(msg_data_char), msg_size);
+    std::string msg_decoded = base64_decode(msg_data_str);
+
+    cv::Mat rawData = cv::Mat(1, msg_decoded.length(), CV_8UC1, (unsigned char*)msg_decoded.c_str());
+    cv::Mat img = cv::imdecode(rawData, -1);
+
+    // do some processing on img
+
+    // Send reply back to client
+    zmq_send(responder, msg_decoded.c_str(), msg_size, 0);
+  }
 }
