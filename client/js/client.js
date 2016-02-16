9691ff7287b9cf73cbfa27ce6280246a4855be2a
Mark Lee <marklee.lhs@gmail.com>
2016-02-16 15:39:22 -0500

    .gitignore fix

--- client/js/client.js
+++ client/js/client.js
@@ -8,14 +8,22 @@ var showMessage = function (msg) {
 
 var canvas = document.getElementById('webcam-feed');
 var context = canvas.getContext('2d');
+var dataCanvas = document.getElementById('data-feed');
+var dataContext = dataCanvas.getContext('2d');
 var image = new Image();
 var loading = true;
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
 
 showMessage('Loading...');
 
 $('.res-option').click(function (event) {
   var option = $(this).data('scale');
   socket.emit('change_resolution', parseFloat(option));
+});
+
+socket.on('config', function (config) {
+  dataContext.scale(canvasWidth / config.width, canvasHeight / config.height);
 });
 
 socket.on('frame', function (data) {
@@ -25,7 +33,25 @@ socket.on('frame', function (data) {
   }
   image.src = 'data:image/png;base64,' + data.buffer;
   // no need for image onload; immediately ready
-  context.drawImage(image, 0, 0, canvas.width, canvas.height);
+  context.drawImage(image, 0, 0, canvasWidth, canvasHeight);
+});
+
+socket.on('frameData', function (data) {
+  dataContext.clearRect(0, 0, canvasWidth, canvasHeight);
+  dataContext.strokeStyle = 'red';
+  data.point.forEach(function (pt) {
+    dataContext.beginPath();
+    dataContext.arc(pt.x, pt.y, Math.abs(pt.t1 - pt.t2), 2 * Math.PI, false);
+    dataContext.stroke();
+  });
+
+  dataContext.strokeStyle = 'blue';
+  data.line.forEach(function (line) {
+    dataContext.beginPath();
+    dataContext.moveTo(line.point[0].x, line.point[0].y);
+    dataContext.lineTo(line.point[1].x, line.point[1].y);
+    dataContext.stroke();
+  });
 });
 
 socket.on('server_shutdown', function () {
